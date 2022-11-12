e9656dbe53467d7b32aa59f69d6b2f61f10592e9
NatuAls <nahuelalesso19@gmail.com>
2022-11-11 21:42:21 -0300

    homework06 complete

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -4,15 +4,57 @@ const app = express();
 app.use(express.json()); // for parsing application/json
 
 app.get('/', (req, res) => {
-  res.send({
+  res.status(200).send({
     message: 'hola',
   });
 });
 
+app.get('/test', (req, res) => {
+  res.status(200).send({message: 'test'});
+});
+
+app.post('/sum', (req, res) => {
+  res.status(200).send({result: req.body.a + req.body.b})
+})
+
 app.post('/product', (req, res) => {
-  res.send({
-    result: req.body.a / req.body.b,
+  res.status(200).send({
+    result: req.body.a * req.body.b,
   });
+});
+
+app.post('/sumArray', (req,res) => {
+  const {array, num} = req.body;
+  let result = false;
+  for(let e of array){
+    for(let x of array){
+      if(e !== x) {
+        if(e + x === num) result = true;
+      }
+    };
+  };
+  res.status(200).send({result});
+});
+
+app.post('/numString', (req, res) => {
+  const {string} = req.body;
+  if(string.length > 0 && typeof string === 'string'){
+    res.status(200).send({result: string.length});
+  }
+  else res.status(400).send({error: 'should be a string'});
+});
+
+app.post('/pluck', (req, res) => {
+  const {array, property} = req.body;
+  if(array instanceof Array && typeof property === 'string'){
+    let result = [];
+    array.forEach(obj => {
+      for(let prop in obj){
+        if(prop === property) result.push(obj[prop])
+      }
+    });
+    res.status(200).send({result: result});
+  } else res.status(400).send({error: 'no se pasaron los datos correctamente'});
 });
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -16,7 +16,7 @@ describe('Test de APIS', () => {
     it('responds with 200', () => agent.get('/test').expect(200));
     it('responds with and object with message `test`', () =>
       agent.get('/test').then((res) => {
-        expect(res.body.message).toEqual('hola');
+        expect(res.body.message).toEqual('test');
       }));
   });
 
@@ -26,7 +26,7 @@ describe('Test de APIS', () => {
       agent.post('/sum')
         .send({a: 2, b: 3})
         .then((res) => {
-          expect(res.body.result).toEqual(5);
+          expect(res.body.result).toBe(5);
         })
     );
   });
@@ -37,19 +37,68 @@ describe('Test de APIS', () => {
       agent.post('/product')
         .send({a: 2, b: 3})
         .then((res) => {
-          expect(res.body.result).toEqual(6);
+          expect(res.body.result).toBe(6);
         })
     );
   });
 
   describe('POST /sumArray', () => {
-    it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
-      agent.post('/sumArray')
+    it('responds with 200', () => agent.post('/sumArray').send({array: [2,1], num: 3}).expect(200));
+    it('responds with true', () => {
+      return agent.post('/sumArray')
         .send({array: [2,5,7,10,11,15,20], num: 13})
         .then((res) => {
-          expect(res.body.result).toEqual(true);
-      }));
+          expect(res.body.result).toBe(true);
+        });
+    });
+    it('responds with false', () => {
+      return agent.post('/sumArray')
+        .send({array: [2,5,7,10,11,15,20], num: 50})
+        .then((res) => {
+          expect(res.body.result).toBe(false);
+        });
+    });
+    it('you shoul not add the same number twice', () => {
+      return agent.post('/sumArray')
+        .send({array: [2,5,7,10,11,15,49], num: 4})
+        .then((res) => {
+          expect(res.body.result).toBe(false);
+        });
+    });
+  });
+
+  describe('POST /numString', () => {
+    it('responds with 200', () => agent.post('/numString').send({string: 'x'}).expect(200));
+    it('responds with 4', () => {
+      return agent.post('/numString')
+        .send({string: 'hola'})
+        .then((res) => {
+          expect(res.body.result).toBe(4);
+        });
+    });
+    it('responds with 400 when there is an empty string' , () =>
+      agent.post('/numString').send({string: ''}).expect(400)
+    );
+    it('respond with 400 when there is a number' , () => {
+      return agent.post('/numString').send({string: 12}).expect(400);
+    });
+  });
+
+  describe('POST /pluck', () => {
+    it('responds with 200', () => agent.post('/pluck').send({array: [{name: 'natu'}], property: 'name'}).expect(200));
+    it('responds with pluck functionality', () => {
+      return agent.post('/pluck')
+        .send({array: [{name: 'natu'}, {age: 24}, {name: 'bruce'}], property: 'name'})
+        .then((res) => {
+          expect(res.body.result).toEqual(['natu', 'bruce']);
+        });
+    });
+    it('responds with 400 when prop is not array' , () =>
+      agent.post('/pluck').send({array: 12, property: ''}).expect(400)
+    );
+    it('respond with 400 when prop is not string' , () =>
+      agent.post('/pluck').send({array: [], property: 12}).expect(400)
+    );
   });
 
 });
